cabcff3a11c67acede535d4c3307be87ce6b60b3
AntGtz <tranetrose@gmail.com>
2022-05-02 14:50:21 -0500

    server

--- app/lib/controllers/ExplorerController.js
+++ app/lib/controllers/ExplorerController.js
@@ -1,20 +1,20 @@
 const ExplorerService = require("./../services/ExplorerService");
 const FizzbuzzService = require("./../services/FizzbuzzService");
 const Reader = require("./../utils/Reader");
-let read = Reader.readJsonFile("./explorers.json")
+let read = Reader.readJsonFile("./explorers.json");
 
 class ExplorerController{
     static getExplorersByMission(mission){
-        let filter = ExplorerService.filterByMission(read, mission)
+        let filter = ExplorerService.filterByMission(read, mission);
         return filter;
     }
     static getExplorersUsernamesByMission(mission){
-        let usernames = ExplorerService.getExplorersUsernameByMission(read, mission)
-        return usernames
+        let usernames = ExplorerService.getExplorersUsernameByMission(read, mission);
+        return usernames;
     }
     static getExplorersAmonutByMission(mission){
-        let amount = ExplorerService.getAmountOfExplorersByMission(read, mission)
-        return amount
+        let amount = ExplorerService.getAmountOfExplorersByMission(read, mission);
+        return amount;
     }
 }
 
